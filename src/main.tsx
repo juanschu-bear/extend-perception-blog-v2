f40ea1c8dcbff992f1a3aea59c23947d605379c2
juanschu-bear <262013142+juanschu-bear@users.noreply.github.com>
2026-03-24 20:54:02 +0100

    Switch app base path to root domain

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/extend-humans-blog">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
